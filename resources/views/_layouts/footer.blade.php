6c1045dc278265846f1151afe2931cabc7fb3fa1
rcanai <canai3jp@gmail.com>
2018-07-25 22:06:21 +0900

    レイアウト調整

--- resources/views/_layouts/footer.blade.php
+++ resources/views/_layouts/footer.blade.php
@@ -1,1 +1,5 @@
-<footer id="footer"></footer>
+<footer id="footer">
+  <div class="container">
+    <small>&copy; 2018 rcanai</small>
+  </div>
+</footer>
--- resources/views/_layouts/master.blade.php
+++ resources/views/_layouts/master.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="author" content="rcanai">
   <meta name="csrf-token" content="{{csrf_token()}}">
   <title>@yield('title') My Laravel blog</title>
   <link rel="icon" href="{{asset('favicon.ico')}}">
